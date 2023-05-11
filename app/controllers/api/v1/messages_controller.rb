f5c3cc8461b35cf1f41d12cfe4cc11a6023bb592
EddyShimwa <shedpray16@gmail.com>
2023-05-11 21:20:20 +0200

    fixed the linter errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,10 +1,10 @@
 module Api
-    module V1
-      class MessagesController < ApplicationController
-        def random
-          message = Message.order("RANDOM()").first
-          render json: { message: message.content }
-        end
+  module V1
+    class MessagesController < ApplicationController
+      def random
+        message = Message.order('RANDOM()').first
+        render json: { message: message.content }
       end
     end
   end
+end
